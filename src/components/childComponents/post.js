695ae0065ab82b421589f275fc4bf1fc2079da62
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-03-16 00:01:10 +0530

    like and unlike post

--- src/components/childComponents/post.js
+++ src/components/childComponents/post.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Comment from "../../static/svg/comment.svg";
 import { Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Moment from "moment";
 
 const Post = (props) => {
+  let [checked, setChecked] = useState(props.liked);
+  const checkChangeHandler = useCallback(
+    (e) => {
+      setChecked(!checked);
+      props.changed(e);
+    },
+    [checked, props]
+  );
   return (
     <div className="card mb-3">
       <div className="card-body card-body___img">
-        <Image
-          className="user-avatar"
-          src="https://media-exp1.licdn.com/dms/image/C5603AQEEYrMorxyHoQ/profile-displayphoto-shrink_200_200/0/1592154989934?e=1620864000&v=beta&t=BMcCRHn7a3HROnr16nFptSE2uWzBrpS-FFqW-phvfyw"
-          roundedCircle
-        />
+        <Image className="user-avatar" src={props.image} roundedCircle />
         <Link to="/" className="username-link">
           <p className="card-title username">{props.userName}</p>
         </Link>
       </div>
-      <img
-        className="card-img-top"
-        src="https://images.unsplash.com/photo-1593640495253-23196b27a87f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzZ8fGRlc2t0b3AlMjBjb21wdXRlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-        alt="Img"
-      />
+      <img className="card-img-top" src={props.image} alt="Img" />
       <div className="card-body">
-        <input type="checkbox" checked={props.liked} />
+        <input
+          type="checkbox"
+          checked={checked}
+          name={props.id}
+          onChange={checkChangeHandler}
+        />
         <img class="svg_page" src={Comment} alt="Comment" />
         <h6 className="card-title">{props.title}</h6>
         <p className="card-text">{props.desc}</p>
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
 import Post from "../components/childComponents/post";
 import { PostContext } from "../context/ReposContext";
 import Pagination from "../components/childComponents/pagination";
@@ -8,19 +8,9 @@ import Modal from "../components/childComponents/Modal";
 
 const Posts = (props) => {
   const [skips, setSkips] = useState(0);
-
-  const skipsInQueryHandler = (e) => {
-    if (e.target.value !== skips) {
-      if (e.target.value === 1) {
-        setSkips(0);
-      } else {
-        setSkips(e.target.value * 10 - 10);
-      }
-    }
-  };
   const GET_ALL_REPOS = gql`
   query {
-    repos(skip: ${skips}, limit: 10) {
+    repos(skip: ${skips}, limit: 5) {
       developer {
         userName
       }
@@ -31,12 +21,43 @@ const Posts = (props) => {
       totalComments
       createdAt
       liked
+      image
     }
   }
 `;
+
+  const LIKE_POST = gql`
+    mutation like($repoId: String!) {
+      like(repoId: $repoId) {
+        number
+      }
+    }
+  `;
+
   const postContext = useContext(PostContext);
 
   const { loading, error, data } = useQuery(GET_ALL_REPOS);
+
+  const [like, payload] = useMutation(LIKE_POST);
+
+  const skipsInQueryHandler = (e) => {
+    if (e.target.value !== skips) {
+      if (e.target.value === 1) {
+        setSkips(0);
+      } else {
+        setSkips(e.target.value * 5 - 5);
+      }
+    }
+  };
+
+  const likeInMutationHandler = (e) => {
+    let id = e.target.name;
+    like({
+      variables: {
+        repoId: id.toString(),
+      },
+    });
+  };
 
   if (loading)
     return (
@@ -57,27 +78,30 @@ const Posts = (props) => {
   if (data) {
     console.log(data);
     postContext.setPosts(data.repos);
-  }
 
-  return (
-    <React.Fragment>
-      {data.repos.map((x) => {
-        return (
-          <Post
-            key={x.id}
-            userName={x.developer.userName}
-            title={x.title}
-            desc={x.desc}
-            likes={x.likes}
-            comments={x.comments}
-            createdAt={x.createdAt}
-            liked={x.liked}
-          />
-        );
-      })}
-      <Pagination clicked={skipsInQueryHandler} />
-    </React.Fragment>
-  );
+    return (
+      <React.Fragment>
+        {data.repos.map((x) => {
+          return (
+            <Post
+              key={x.id}
+              id={x.id}
+              userName={x.developer.userName}
+              title={x.title}
+              desc={x.desc}
+              image={x.image}
+              likes={x.likes}
+              comments={x.comments}
+              createdAt={x.createdAt}
+              liked={x.liked}
+              changed={likeInMutationHandler}
+            />
+          );
+        })}
+        <Pagination clicked={skipsInQueryHandler} />
+      </React.Fragment>
+    );
+  }
 };
 
 export default Posts;
